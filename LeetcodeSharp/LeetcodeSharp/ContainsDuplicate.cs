779bb5ab0c9aeb8cca5eccacd711e59000968e7f
Kanjie <lukanjie@hotmail.com>
2015-09-01 19:50:50 +0800

    Add two solutions
    Add two solutions

---
+++ LeetcodeSharp/LeetcodeSharp/ContainsDuplicate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/contains-duplicate/
+    class ContainsDuplicateSolution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            if (nums.Length == 0 || nums.Length == 1)
+                return false;
+            HashSet<int> set = new HashSet<int>(nums);
+            return set.Count < nums.Length;
+        }
+    }
+}
---
+++ LeetcodeSharp/LeetcodeSharp/SingleNumberIII.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/single-number-iii/
+    class SingleNumberIII
+    {
+        public int[] SingleNumber(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (set.Contains(num))
+                    set.Remove(num);
+                else
+                    set.Add(num);
+            }
+            return set.ToArray();
+        }
+    }
+}
