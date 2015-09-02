330c87db2eca2f73060aa1ef9c54940114d7dbad
Kanjie <lukanjie@hotmail.com>
2015-09-02 14:24:10 +0800

    ContainsDuplicateII, MergeSortedArray, PlusOne
    ContainsDuplicateII, MergeSortedArray, PlusOne

---
+++ LeetcodeSharp/LeetcodeSharp/ContainsDuplicateSolutionII.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    public class ContainsDuplicateSolutionII
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            if (nums.Length < 2 || k < 1)
+                return false;
+            k = Math.Min(nums.Length - 1, k);
+            var hashset = new HashSet<int>();
+            for (int i = 0; i < k + 1; i++)
+            {
+                if (!hashset.Add(nums[i]))
+                    return true;
+            }
+            var count = nums.Length - k;
+            for (int index = 1; index < count; index++)
+            {
+                hashset.Remove(nums[index - 1]);
+                if (!hashset.Add(nums[index + k]))
+                    return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ LeetcodeSharp/LeetcodeSharp/PlusOne.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/plus-one/
+    public class PlusOneSolution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            int pointer = digits.Length - 1;
+            while (true)
+            {
+                if (digits[pointer] < 9)
+                {
+                    digits[pointer]++;
+                    return digits;
+                }
+                else
+                {
+                    //reach the head
+                    if (pointer == 0)
+                    {
+                        digits[pointer] = 0;
+                        var newdigits = new int[digits.Length + 1];
+                        newdigits[0] = 1;
+                        for (int i = 0; i < digits.Length; i++)
+                            newdigits[i + 1] = digits[i];
+                        return newdigits;
+                    }
+                    else
+                    {
+                        digits[pointer] = 0;
+                        pointer--;
+                    }
+                }
+            }
+        }
+    }
+}
