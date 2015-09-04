ea26083fe9b9aeb7a6f0b80ac514cf0a86b204b6
partboy <lukanjie@hotmail.com>
2015-09-04 22:55:31 +0800

    Add IsomorphicStrings

---
+++ LeetcodeSharp/LeetcodeSharp/IsomorphicStrings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/isomorphic-strings/
+    public class IsomorphicStrings
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            if (s == t)
+                return true;
+            //key is char in t, value is char in s
+            Dictionary<char, char> mapping = new Dictionary<char, char>();
+            HashSet<char> set = new HashSet<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (mapping.ContainsKey(s[i]))
+                {
+                    if (t[i] != mapping[s[i]])
+                        return false;
+                }
+                else
+                {
+                    mapping.Add(s[i], t[i]);
+                    if (!set.Add(t[i]))
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
