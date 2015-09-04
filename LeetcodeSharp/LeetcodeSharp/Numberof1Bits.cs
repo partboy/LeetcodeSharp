000b021a71c74ccdce2ed3a481b83612f4586ea6
partboy <lukanjie@hotmail.com>
2015-09-04 23:02:07 +0800

    Add Numberof1Bits

---
+++ LeetcodeSharp/LeetcodeSharp/Numberof1Bits.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/number-of-1-bits/
+    public class Numberof1Bits
+    {
+        public int HammingWeight(uint n)
+        {
+            int count = 0;
+            while (n != 0)
+            {
+                if (n % 2 == 1)
+                    count++;
+                n = n / 2;
+            }
+            return count;
+        }
+    }
+}
