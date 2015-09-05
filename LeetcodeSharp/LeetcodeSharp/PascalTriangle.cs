a6679968ac088a35f4f03599330c072531e13258
partboy <lukanjie@hotmail.com>
2015-09-05 10:13:23 +0800

    add PascalTriangle

---
+++ LeetcodeSharp/LeetcodeSharp/PascalTriangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/pascals-triangle/
+    public class PascalTriangle
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            List<IList<int>> triangle = new List<IList<int>>(numRows);
+            for (int index = 0; index < numRows; index++)
+            {
+                int[] row = new int[index + 1];
+                row[0] = 1;
+                row[index] = 1;
+                if (triangle.Count > 0)
+                {
+                    var lastrow = triangle.Last();
+                    for (int i = 1; i < index; i++)
+                        row[i] = lastrow[i - 1] + lastrow[i];
+                }
+                triangle.Add(row);
+            }
+            return triangle;
+        }
+    }
+}
---
+++ LeetcodeSharp/LeetcodeSharp/PascalTriangleII.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/pascals-triangle-ii/
+    public class PascalTriangleII
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            int[] row = new int[rowIndex + 1];
+            row[0] = 1;
+            for (int round = 0; round <= rowIndex; round++)
+            {
+                for (int i = round; i >= 1; i--)
+                {
+                    row[i] = row[i] + row[i - 1];
+                }
+            }
+            return row;
+        }
+    }
+}
