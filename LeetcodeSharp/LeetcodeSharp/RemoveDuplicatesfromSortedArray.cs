8d0562d3e7e691686203b959ba047949446b20b9
partboy <lukanjie@hotmail.com>
2015-09-06 23:49:36 +0800

    add removeduplicatesfromsortarray add reverseInteger

---
+++ LeetcodeSharp/LeetcodeSharp/RemoveDuplicatesfromSortedArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+    public class RemoveDuplicatesfromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0 || nums.Length == 1)
+                return nums.Length;
+            int ph = nums[0];
+            int count = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[i - 1])
+                {
+                    nums[i - 1] = ph;
+                    count++;
+                }
+            }
+            int firstPossible = 1;
+            for (int i = 1; i < nums.Length - count; i++)
+            {
+                if (nums[i] == ph)
+                {
+                    for (int j = Math.Max(i + 1, firstPossible); j < nums.Length; j++)
+                    {
+                        if (nums[j] != ph)
+                        {
+                            var temp = nums[j];
+                            nums[j] = ph;
+                            nums[i] = temp;
+                            firstPossible = j + 1;
+                            break;
+                        }
+                    }
+                }
+            }
+            return nums.Length - count;
+        }
+    }
+}
---
+++ LeetcodeSharp/LeetcodeSharp/ReverseInteger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/reverse-integer/
+    public class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            List<int> list = new List<int>();
+            while (x != 0)
+            {
+                list.Add(x % 10);
+                x /= 10;
+            }
+            long number = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                number = number * 10;
+                number += list[i];
+            }
+            if (number > int.MaxValue || number < int.MinValue)
+                return 0;
+            return (int)number;
+        }
+    }
+}
