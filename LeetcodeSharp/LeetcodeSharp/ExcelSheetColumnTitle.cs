1c36f1c538a044cfce45361eaf6e3356bb2773dd
partboy <lukanjie@hotmail.com>
2015-09-07 08:11:47 +0800

    add ExcelSheetColumnTitle

---
+++ LeetcodeSharp/LeetcodeSharp/ExcelSheetColumnTitle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/excel-sheet-column-title/
+    public class ExcelSheetColumnTitle
+    {
+        public string ConvertToTitle(int n)
+        {
+            List<char> title = new List<char>(7);
+            while (n > 0)
+            {
+                var remainder = n % 26;
+                char letter = remainder == 0 ? 'Z' : (char)('A' - 1 + remainder);
+                title.Add(letter);
+                n = n - (remainder == 0 ? 26 : remainder);
+                n = n / 26;
+            }
+            title.Reverse();
+            return new String(title.ToArray());
+        }
+    }
+}
