2ede17f9e8499013cfb270b6979a5202d74e1f26
partboy <lukanjie@hotmail.com>
2015-09-08 23:22:07 +0800

    add FirstBadVersion add LongestCommonPrefix

---
+++ LeetcodeSharp/LeetcodeSharp/FirstBadVersion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    public class FirstBadVersionSolution : VersionControl
+    {
+        public int FirstBadVersion(int n)
+        {
+            if (IsBadVersion(0))
+                return 0;
+            return BinarySearchFirstBadVersion(0, n);
+        }
+
+        private int BinarySearchFirstBadVersion(int head, int tail)
+        {
+            if (head == tail - 1)
+                return tail;
+            int mid = (int)(((long)head + (long)tail) / 2);
+            bool isbad = IsBadVersion(mid);
+            if (isbad)
+                return BinarySearchFirstBadVersion(head, mid);
+            else
+                return BinarySearchFirstBadVersion(mid, tail);
+        }
+    }
+
+    public class VersionControl
+    {
+        public bool IsBadVersion(int version)
+        {
+            return version >= 1702766719;
+        }
+    }
+}
---
+++ LeetcodeSharp/LeetcodeSharp/LongestCommonPrefix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    public class LongestCommonPrefixSolution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0 || strs.Any(x => string.IsNullOrEmpty(x)))
+                return string.Empty;
+            var minlength = strs.Min(x => x.Length);
+            StringBuilder prefixBuilder = new StringBuilder();
+            for (int i = 0; i < minlength; i++)
+            {
+                var charactar = strs[0][i];
+                if (strs.All(x => x[i] == charactar))
+                    prefixBuilder.Append(charactar);
+                else
+                    break;
+            }
+            return prefixBuilder.ToString();
+        }
+    }
+}
