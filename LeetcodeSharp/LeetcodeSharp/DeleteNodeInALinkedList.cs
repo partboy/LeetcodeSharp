69807512d3b78c0e2b5d5ce1253d16d1f14119a1
partboy <lukanjie@hotmail.com>
2015-09-27 10:06:42 +0800

    add DeleteNodeInALinkedList

---
+++ LeetcodeSharp/LeetcodeSharp/DeleteNodeInALinkedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/delete-node-in-a-linked-list/
+    public class DeleteNodeInALinkedList
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
