6c4e252a2051c34b031b8dd82ad414bce20a4729
partboy <lukanjie@hotmail.com>
2015-10-06 10:02:21 +0800

    add remove element add remove nth node from end of list

---
+++ LeetcodeSharp/LeetcodeSharp/RemoveElement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/remove-element/
+    public class RemoveElementSolution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+            int tail = nums.Length - 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == val)
+                {
+                    while (true)
+                    {
+                        if (tail <= i)
+                            return i;
+                        if (nums[tail] != val)
+                        {
+                            nums[i] = nums[tail--];
+                            break;
+                        }
+                        else
+                            tail--;
+                    }
+
+                }
+                if (tail <= i)
+                    return i + 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ LeetcodeSharp/LeetcodeSharp/RemoveNthNodeFromEndofList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+    public class RemoveNthNodeFromEndofList
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode t = head;
+            ListNode p = head;
+            if (head == null)
+                return null;
+            else if (head.next == null && n > 0)
+                return null;
+            for (int i = 0; i < n - 1; i++)
+                t = t.next;
+            if (t.next == null)
+                return head.next;
+            else
+                t = t.next;
+            while (true)
+            {
+                if (t.next == null)
+                {
+                    if (p.next == head)
+                        return head.next;
+                    else
+                        p.next = p.next.next;
+                    break;
+                }
+                else
+                {
+                    p = p.next;
+                    t = t.next;
+                }
+            }
+            return head;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+    }
+}
