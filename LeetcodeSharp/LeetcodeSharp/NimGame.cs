82801e22e0dcf248a8a76bdda1aead03b5efbb49
Kanjie <lukanjie@hotmail.com>
2015-11-03 10:12:13 +0800

    add NimGame

---
+++ LeetcodeSharp/LeetcodeSharp/NimGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/nim-game/
+    public class NimGame
+    {
+        public bool CanWinNim(int n)
+        {
+            return n % 4 != 0;
+        }
+    }
+}
