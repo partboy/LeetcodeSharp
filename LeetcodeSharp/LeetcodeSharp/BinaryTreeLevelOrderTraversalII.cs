b0b0a13e4c3ae704a3d159f60b14bea796c9be61
partboy <lukanjie@hotmail.com>
2015-11-05 08:43:07 +0800

    add DeleteNodeInALinkedList

---
+++ LeetcodeSharp/LeetcodeSharp/BinaryTreeLevelOrderTraversalII.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+    public class BinaryTreeLevelOrderTraversalII
+    {
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            if (root == null)
+                return new List<IList<int>>();
+            List<IList<int>> result = new List<IList<int>>();
+            var last = new List<TreeNode>() { root };
+            while (true)
+            {
+                var currentNodes = new List<TreeNode>();
+                var currentResult = last.Select(x => x.val).ToList();
+                result.Add(currentResult);
+                foreach (var item in last)
+                {
+                    if (item.left != null)
+                        currentNodes.Add(item.left);
+                    if (item.right != null)
+                        currentNodes.Add(item.right);
+                }
+                if (currentNodes.Count == 0)
+                    break;
+                else
+                    last = currentNodes;
+            }
+            result.Reverse();
+            return result;
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
