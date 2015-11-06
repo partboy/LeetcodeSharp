46f31bb47e16ee0e09f53e29c0771d1248c1f6cc
Kanjie <lukanjie@hotmail.com>
2015-11-06 17:52:56 +0800

    add binarytreepaths, palindromeLinkedList

---
+++ LeetcodeSharp/LeetcodeSharp/BinaryTreePaths.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/binary-tree-paths/
+    public class BinaryTreePathsSolution
+    {
+        public IList<string> BinaryTreePaths(TreeNode root)
+        {
+            var result = new List<string>();
+            if (root == null)
+                return result;
+            else
+                return GetPaths(root);
+
+        }
+
+        private IList<string> GetPaths(TreeNode node)
+        {
+            if (node.left == null && node.right == null)
+                return new List<string>() { node.val.ToString() };
+            else if (node.left == null)
+                return GetPaths(node.right).Select(x => string.Format("{0}->{1}", node.val, x)).ToList();
+            else if (node.right == null)
+                return GetPaths(node.left).Select(x => string.Format("{0}->{1}", node.val, x)).ToList();
+            else
+            {
+                var left = GetPaths(node.left).Select(x => string.Format("{0}->{1}", node.val, x));
+                var right = GetPaths(node.right).Select(x => string.Format("{0}->{1}", node.val, x));
+                return left.Concat(right).ToList();
+            }
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
---
+++ LeetcodeSharp/LeetcodeSharp/PalindromeLinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/palindrome-linked-list/
+    public class PalindromeLinkedList
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            bool isover = false;
+            return Check(head, ref head, ref isover);
+        }
+
+        private bool Check(ListNode current, ref ListNode head, ref bool isover)
+        {
+            //start to compare, it's the end of the linkedlist
+            if (current == null)
+                return true;
+            var result = Check(current.next, ref head, ref isover);
+            if (result == false)
+                return false;
+            else if (isover)
+                return true;
+            else if (current.val != head.val)
+                return false;
+            else
+            {
+                if (head == current || head.next == current)
+                    isover = true;
+                else
+                    head = head.next;
+                return true;
+            }
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+    }
+}
