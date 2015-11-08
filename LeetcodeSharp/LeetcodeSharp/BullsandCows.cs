fbde4e2170e7cf2aee9737662050f85927e1ae2b
partboy <lukanjie@hotmail.com>
2015-11-08 22:17:39 +0800

    add BullsandCows add ImplementstrStr

---
+++ LeetcodeSharp/LeetcodeSharp/BullsandCows.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/bulls-and-cows/
+    public class BullsandCows
+    {
+        public string GetHint(string secret, string guess)
+        {
+            int countA = 0;
+            int[] secretList = new int[10];
+            int[] guessList = new int[10];
+            for (int i = 0; i < secret.Length; i++)
+            {
+                if (secret[i] == guess[i])
+                    countA++;
+                else
+                {
+                    secretList[secret[i] - '0']++;
+                    guessList[guess[i] - '0']++;
+                }
+            }
+            if (countA == secret.Length)
+                return string.Format("{0}A0B", countA);
+            int countB = 0;
+            for (int i = 0; i < 10; i++)
+                countB += Math.Min(secretList[i], guessList[i]);
+            return string.Format("{0}A{1}B", countA, countB);
+        }
+    }
+}
---
+++ LeetcodeSharp/LeetcodeSharp/ImplementstrStr.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/implement-strstr/
+    public class ImplementstrStr
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (haystack == null || needle == null)
+                return -1;
+            else if (needle == string.Empty)
+                return 0;
+            else if (needle.Length > haystack.Length)
+                return -1;
+            else
+            {
+                for (int i = 0; i < haystack.Length && haystack.Length - i >= needle.Length; i++)
+                {
+                    if (haystack[i] == needle[0])
+                    {
+                        if (needle.Length > 1)
+                        {
+                            for (int j = 1; j < needle.Length; j++)
+                            {
+                                if (haystack[i + j] != needle[j])
+                                    break;
+                                else if (j == needle.Length - 1)
+                                    return i;
+                                else
+                                    continue;
+                            }
+                        }
+                        else
+                            return i;
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+}
