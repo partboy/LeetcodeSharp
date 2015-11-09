9355a107fb08306a17335c7529e896679db752f9
partboy <lukanjie@hotmail.com>
2015-11-09 22:40:08 +0800

    add besttimetobuyandsellstocks

---
+++ LeetcodeSharp/LeetcodeSharp/BestTimetoBuyandSellStock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    public class BestTimetoBuyandSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length < 2)
+                return 0;
+            int min = prices[0];
+            int profit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < min)
+                    min = prices[i];
+                else
+                    profit = Math.Max(profit, prices[i] - min);
+            }
+            return profit;
+        }
+    }
+}
