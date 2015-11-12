3e282b334f9bba55afa4355c32f93acd985dd6ac
partboy <lukanjie@hotmail.com>
2015-11-12 23:30:11 +0800

    add RangeSumQueryImmutable

---
+++ LeetcodeSharp/LeetcodeSharp/RangeSumQueryImmutable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/range-sum-query-immutable/
+    public class RangeSumQueryImmutable
+    {
+        private int[] sums;
+
+        public RangeSumQueryImmutable(int[] nums)
+        {
+            sums = new int[nums.Length + 1];
+            for (int i = 1; i < sums.Length; i++)
+                sums[i] = sums[i - 1] + nums[i - 1];
+        }
+
+        public int SumRange(int i, int j)
+        {
+            return sums[j + 1] - sums[i];
+        }
+    }
+}
