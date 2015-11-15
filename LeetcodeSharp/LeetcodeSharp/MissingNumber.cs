febe4dfc88ae26949d303fff6d3ba2d92e252010
partboy <lukanjie@hotmail.com>
2015-11-15 23:14:19 +0800

    add MissingNumberSolution

---
+++ LeetcodeSharp/LeetcodeSharp/MissingNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/missing-number/
+    public class MissingNumberSolution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            long sum = 0;
+            int max = 0;
+            bool zerofound = false;
+            foreach (var num in nums)
+            {
+                sum += num;
+                if (num > max)
+                    max = num;
+                if (num == 0)
+                    zerofound = true;
+            }
+            if (!zerofound)
+                return 0;
+            long newsum = max * (max + 1) / 2;
+            if (newsum > sum)
+                return (int)(newsum - sum);
+            else
+                return max + 1;
+        }
+    }
+}
