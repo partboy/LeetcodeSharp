e8b58d85491701b93d5fda648ed20e9a87d4e3d0
partboy <lukanjie@hotmail.com>
2015-11-21 08:56:58 +0800

    add IntegertoRoman, MaximumSubarraySolution

---
+++ LeetcodeSharp/LeetcodeSharp/IntegertoRoman.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/integer-to-roman/
+    public class IntegertoRoman
+    {
+        public string IntToRoman(int num)
+        {
+            StringBuilder builder = new StringBuilder();
+            while (num >= 1000)
+            {
+                num -= 1000;
+                builder.Append('M');
+            }
+            while (num >= 100)
+            {
+                num -= 100;
+                builder.Append('C');
+            }
+            while (num >= 10)
+            {
+                num -= 10;
+                builder.Append('X');
+            }
+            while (num > 0)
+            {
+                num -= 1;
+                builder.Append('I');
+            }
+            var result = builder.ToString();
+            result = result.Replace("CCCCCCCCC", "CM");
+            result = result.Replace("CCCCC", "D");
+            result = result.Replace("CCCC", "CD");
+            result = result.Replace("XXXXXXXXX", "XC");
+            result = result.Replace("XXXXX", "L");
+            result = result.Replace("XXXX", "XL");
+            result = result.Replace("IIIIIIIII", "IX");
+            result = result.Replace("IIIII", "V");
+            result = result.Replace("IIII", "IV");
+            return result;
+        }
+    }
+}
---
+++ LeetcodeSharp/LeetcodeSharp/MaximumSubarray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/maximum-subarray/
+    public class MaximumSubarraySolution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+            int max = nums[0];
+            int maxthisround = 0;
+            int sum = 0;
+            int index = 0;
+            while (index < nums.Length)
+            {
+                sum = 0;
+                maxthisround = nums[index];
+                for (; index < nums.Length; index++)
+                {
+                    var newsum = sum + nums[index];
+                    if (newsum <= 0)
+                    {
+                        max = Math.Max(max, maxthisround);
+                        index++;
+                        break;
+                    }
+                    else
+                    {
+                        maxthisround = Math.Max(maxthisround, newsum);
+                        sum = newsum;
+                    }
+                }
+            }
+            return Math.Max(max, maxthisround);
+        }
+    }
+}
+
