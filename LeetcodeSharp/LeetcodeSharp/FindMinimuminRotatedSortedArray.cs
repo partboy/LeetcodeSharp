20535bd4b9ad3f256976dd0f13ab16dcaf22896f
partboy <lukanjie@hotmail.com>
2015-11-21 09:38:55 +0800

    add FindMinimuminRotatedSortedArray

---
+++ LeetcodeSharp/LeetcodeSharp/FindMinimuminRotatedSortedArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+    public class FindMinimuminRotatedSortedArray
+    {
+        public int FindMin(int[] nums)
+        {
+            int start = 0;
+            int end = nums.Length - 1;
+            while (start + 1 < end)
+            {
+                var target = nums[start];
+                var mid = (start + end) / 2;
+                if (nums[mid] > target)
+                    start = mid;
+                else
+                    end = mid;
+            }
+            return Math.Min(nums[0], nums[end]);
+        }
+    }
+}
