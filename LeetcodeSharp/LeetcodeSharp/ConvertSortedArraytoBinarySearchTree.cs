fa8fd9cc73dff00806c8796597a1990dd70f63e1
partboy <lukanjie@hotmail.com>
2015-11-22 13:29:25 +0800

    add uniquepaths, convertsortedarraytobst

---
+++ LeetcodeSharp/LeetcodeSharp/ConvertSortedArraytoBinarySearchTree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+    public class ConvertSortedArraytoBinarySearchTree
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return null;
+            return SortedArrayToBSTInternal(nums, 0, nums.Length - 1);
+        }
+
+        public TreeNode SortedArrayToBSTInternal(int[] nums, int low, int high)
+        {
+            if (low > high)
+                return null;
+            if (low == high)
+                return new TreeNode(nums[low]);
+            else
+            {
+                var mid = (low + high) / 2;
+                TreeNode root = new TreeNode(nums[mid]);
+                root.left = SortedArrayToBSTInternal(nums, low, mid - 1);
+                root.right = SortedArrayToBSTInternal(nums, mid + 1, high);
+                return root;
+            }
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
---
+++ LeetcodeSharp/LeetcodeSharp/UniquePaths.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/unique-paths/
+    public class UniquePathsSolution
+    {
+        //PICK N FROM M+N C(n+m,n)=(m+n)!/n!/m!
+        public int UniquePaths(int m, int n)
+        {
+            if (m < 1 || n < 1)
+                return 0;
+            if (m == 1 || n == 1)
+                return 1;
+            int p = m <= n ? n - 1 : m - 1;
+            int q = m <= n ? m - 1 : n - 1;
+            long value1 = 1;
+            long value2 = 1;
+            for (int i = 1; i <= q; i++)
+            {
+                value1 *= i;
+                value2 *= (p + i);
+            }
+            return (int)(value2 / value1);
+        }
+    }
+}
