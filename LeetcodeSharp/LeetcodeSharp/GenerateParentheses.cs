79a5de4b154b91ec14b73b994ac2b44517c57bfd
partboy <lukanjie@hotmail.com>
2015-11-24 08:42:50 +0800

    add GenerateParentheses

---
+++ LeetcodeSharp/LeetcodeSharp/GenerateParentheses.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSharp
+{
+    //https://leetcode.com/problems/generate-parentheses/
+    //ref:https://leetcode.com/discuss/14436/concise-recursive-c-solution
+    public class GenerateParentheses
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            List<string> list = new List<string>();
+            AddParentheses(list, string.Empty, n, n);
+            return list;
+        }
+
+        private void AddParentheses(List<string> list, string str, int left, int right)
+        {
+            if (left == 0 && right == 0)
+            {
+                list.Add(str);
+                return;
+            }
+            if (left > 0)
+                AddParentheses(list, str + "(", left - 1, right);
+            if (right > left)
+                AddParentheses(list, str + ")", left, right - 1);
+        }
+    }
+}
